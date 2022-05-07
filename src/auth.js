53a2baa203f54d89454a39fe6bbe6c988ab1a442
willis444 <willischangkaixun@gmail.com>
2022-05-07 15:51:59 +0800

    added register duplicate id verification

--- src/auth.js
+++ src/auth.js
@@ -10,25 +10,25 @@ const key = process.env.key; //get private key from the env file
 router.post('/login', async function(req, res, err){
     client = await dbConnection.getDb(); //get connection instance
     db = client.db('Project_Health'); //point to spicific db
-    if(!req.body.id || !req.body.password){ // input validation
+    if(!req.body.user_id || !req.body.user_password){ // input validation
         return res.status(422).send('Missing parameters. The required parameters are id and password.'); //send error message for missing parameters
     }
 
-    result = await db.collection("user").findOne({'id': req.body.id}, function(err, result) { //find the data from the database
+    result = await db.collection("user").findOne({'user_id': req.body.user_id}, function(err, result) { //find the data from the database
         if (err) throw err;
         if (!result) {
             return res.status(401).send('This id is not associated with any account.');
         } else {
-                bcrypt.compare(req.body.password, result.password, function(err, result) { //check if the password inputted by user is the same as the password stored in db
-                    if (result === true) { // if it is true, return true
-                        jwt.sign( {id: req.body.id} , key, { algorithm: 'HS256', expiresIn: '7d' }, async function(err, token) { //sign the user id using jwt token
+                bcrypt.compare(req.body.user_password, result.user_password, function(err, compare_result) { //check if the password inputted by user is the same as the password stored in db
+                    if (compare_result === true) { // if it is true, return true
+                        jwt.sign( {id: result.user_id} , key, { algorithm: 'HS256', expiresIn: '7d' }, async function(err, token) { //sign the user id using jwt token
                             if (err) throw err;
-                            res.status(200).send({'token': token});
+                            return res.status(200).send({'token': token});
                           });
-                    } else if (result === false) { //if the password is wrong, send error message
-                        res.status(403).send("Wrong password");
+                    } else if (compare_result === false) { //if the password is wrong, send error message
+                        return res.status(403).send("Wrong password");
                     } else {
-                        res.status(500).send("Server error, please try again later");
+                        return res.status(500).send("Server error, please try again later");
                     }
                 });
             }
@@ -38,20 +38,26 @@ router.post('/login', async function(req, res, err){
 router.post('/register', async function(req, res){
     try {
         client = await dbConnection.getDb(); //get connection instance
-        if(!req.body.id || !req.body.password){ // input validation
+        if(!req.body.user_id || !req.body.user_password){ // input validation
             return res.status(422).send('Missing parameters. The required parameters are id and password.'); //send error message for missing parameters
+        } else {
+            db = client.db('Project_Health'); //point to spicific db
+            response = await db.collection("user").findOne({'user_id': req.body.user_id});
+            if (response) {
+                return res.status('409').send('Duplicated ID. Please choose another ID.');
+            } else{
+                bcrypt.genSalt(saltRounds, function(err, salt) {
+                    bcrypt.hash(req.body.user_password, salt, async function(err, hash) {
+                        var data = { // define the databody
+                            "user_id": req.body.user_id,
+                            "user_password": hash,
+                        }
+                        await db.collection('user').insertOne(data); //insert the data into the database
+                        return res.status(200).send('Register Successful');
+                    });
+                });
+            }
         }
-        db = client.db('Project_Health'); //point to spicific db
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            bcrypt.hash(req.body.password, salt, async function(err, hash) {
-                var data = { // define the databody
-                    "id": req.body.id,
-                    "password": hash,
-                }
-                await db.collection('user').insertOne(data); //insert the data into the database
-                return res.status(500).send('Register Successful');
-            });
-        });
     } catch (error) {
         console.log({"ERROR": error})
     }
