bf53ab8bd566616f09f376190517b17a1fcba6c4
willis444 <willischangkaixun@gmail.com>
2022-05-19 01:50:29 +0800

    modified add food function and added view log function

--- src/food.js
+++ src/food.js
@@ -72,25 +72,63 @@ router.get('/findFood/:query/:language', async function(req, res){ // special fu
 });
 
  router.post('/LogFood', authenticateToken, async function(req, res){
+    var food = [];
     if (!req.body.datetime || !req.body.meal_type ||!req.body.food_id || !req.body.serving_size) {
         return res.status(422).send('Missing parameters.'); //send error message for missing parameters
     }
+    var ObjectId = require('mongodb').ObjectId; // function to convert string into object id
+    req.body.food_id.forEach(element => food.push(new ObjectId(element))); // loop the food id variable and convert the id from stiring into object id
     client = await dbConnection.getDb(); //get connection instance
     db = client.db('Project_Health'); //point to spicific db
     // define data structure
+    const date = new Date (req.body.datetime); // convert the date from string to date data type
     const data = {
         "user_id": req.user.id, // get user id from jwt token
-        "log_datetime": req.body.datetime,
+        "log_datetime": date,
         "log_meal_type": req.body.meal_type,
-        "log_food_id": req.body.food_id,
+        "log_food_id": food,
         "log_serving_size": req.body.serving_size,
     }
     try {
-    const result = await db.collection("log").insertOne(data); // insert data into the db
+    const result = await db.collection("log").insertOne(data); // insert data into the log collection
     return res.status(200).send(result);
     } catch (error) {
         console.log(error);
     }
 });
+
+router.get('/retrieveLogByDay/:startdate/:endate', authenticateToken, async function(req, res) {
+    var data = []; // declare an array to hold the result
+    client = await dbConnection.getDb(); //get connection instance
+    db = client.db('Project_Health'); //point to spicific db
+    try {
+        const startdate = new Date (req.params.startdate); // convert string into date
+        const endate = new Date (req.params.endate); // convert string into date
+        const result = await db.collection("log").aggregate(
+        [ //aggregrate function
+            {
+                $match: { // match operator to filter out document
+                    '$and': [ // and operator to match all condition
+                        {user_id: (req.user.id)}, // get user id from jwt token
+                        {log_datetime: {$gte: startdate}}, // specify start date
+                        {log_datetime: {$lt: endate}} // specify end date
+                    ]
+                }},
+            {
+                $lookup: { // lookup operator to perform left join
+                    from: "food", // destination collection
+                    localField: "log_food_id", // local field in this collection
+                    foreignField: "_id", // foreign field in the destination collection
+                    as: "food_details" // create a new array to hold the data
+                }
+            }
+        ])
+        await result.forEach(element => data.push(element));
+        return res.status(200).send(data); // return the array to the user
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("server error");
+    }
+})
 
 module.exports = router;
